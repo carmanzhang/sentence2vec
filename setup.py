15750af5493b9c285b6838f28613cd51240fa7d0
v-ramachandran <v.ram28@gmail.com>
2014-12-07 23:15:22 -0600

    Adding a setup file so that this can be used as a dependency

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "sentence2vec",
+    version = 1.0,
+    author = "klb3713",
+    author_email = "klbgyx7@gmail.com",
+    url = "http://klb3713",
+    install_requires=["scipy", "six"]
+)
