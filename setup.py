c4d17026045f556063cef26fb0a8834c57c2b4a9
v-ramachandran <v.ram28@gmail.com>
2014-12-11 03:44:23 -0600

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 packages=['sentence2vec']
 
@@ -10,6 +11,7 @@ setup(
     author_email = "klbgyx7@gmail.com",
     url = "http://klb3713",
     install_requires = ["scipy", "six", "gensim"],
+    include_dirs = [numpy.get_include()],
     ext_modules = cythonize("word2vec_inner.pyx"),
     packages = packages
 )
